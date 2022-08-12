56c2c4741c2e095436e19b03fea73a40f5ef72ee
Alfie <418983798@qq.com>
2022-08-12 14:51:05 +0800

    first commit

---
+++ DoraemonExtension/Classes/DoraemonNSLogManager+Extension.h
@@ -0,0 +1,16 @@
+//
+//  DoraemonNSLogManager+Extension.h
+//  DoraemonExtension
+//
+//  Created by alfie on 2022/8/12.
+//
+
+#import <DoraemonKit/DoraemonKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DoraemonNSLogManager (Extension)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DoraemonExtension/Classes/DoraemonNSLogManager+Extension.m
@@ -0,0 +1,12 @@
+//
+//  DoraemonNSLogManager+Extension.m
+//  DoraemonExtension
+//
+//  Created by alfie on 2022/8/12.
+//
+
+#import "DoraemonNSLogManager+Extension.h"
+
+@implementation DoraemonNSLogManager (Extension)
+
+@end
